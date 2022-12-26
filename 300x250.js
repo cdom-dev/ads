878d5c3322c739cbac3c6b54c9ea309f945b61f8
cdom-dev <119987894+cdom-dev@users.noreply.github.com>
2022-12-26 13:02:02 +0700

    Create 300x250.js

---
+++ 300x250.js
@@ -0,0 +1,2 @@
+atOptions = { 'key' : '0b2246e0fcb7b54b6eb80324ffac81a3', 'format' : 'iframe', 'height' : 250, 'width' : 300, 'params' : {} };
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/0b2246e0fcb7b54b6eb80324ffac81a3/invoke.js"></scr' + 'ipt>');
